1bf88d5af74f611cbb446a9ed0973da18752498a
mohamedriham93 <156790228+mohamedriham93@users.noreply.github.com>
2024-01-25 16:29:47 +0530

    Add files via upload

---
+++ Q8.py
@@ -0,0 +1,13 @@
+int1 = int(input("Enter number1: "))
+int2 = int(input("Enter number2: "))
+
+if int1 == int2 :
+    output = "Equal"
+
+elif int1 < int2 :
+    output =  "int1 is less than int2"
+
+else :
+    output = "int1 is greater than int2"
+
+print (output)

cc6abc61eaaab2f0e90f5b1011945ad1f6758df3
mohamedriham93 <156790228+mohamedriham93@users.noreply.github.com>
2024-01-25 16:34:21 +0530

    Add files via upload

---
+++ Q9.py
@@ -0,0 +1,12 @@
+input = int(input("Enter a number: "))
+
+if input == 0:
+    output = "Equal to zero"
+
+elif input > 0:
+    output = "Positive"
+
+else :
+    output = "Negative"
+
+print (output)

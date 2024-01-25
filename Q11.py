ce9cc414c3ebfd707de4829d6708ec35353fb3c1
mohamedriham93 <156790228+mohamedriham93@users.noreply.github.com>
2024-01-25 16:53:28 +0530

    Add files via upload

---
+++ Q11.py
@@ -0,0 +1,25 @@
+input = int(input("Enter the number of the day in a week: "))
+
+if input == 1:
+    print ("Monday")
+
+elif input == 2:
+    print ("Tuesday")
+
+elif input == 3:
+    print ("Wednesday")
+
+elif input == 4:
+    print ("Thursday")
+
+elif input == 5:
+    print ("Friday")
+
+elif input == 6:
+    print ("Saturdau")
+
+elif input == 7:
+    print ("Sunday")
+
+else :
+    print ("The number you entered is not exist")
